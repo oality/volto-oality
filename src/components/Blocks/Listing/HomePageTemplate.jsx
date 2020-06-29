e73b8f5a1c0a573784a724f699187a5225ff2741
Benoit Suttor <bsuttor@imio.be>
2020-06-29 18:23:04 +0200

    fix: use semantic ui to build image

--- src/components/Blocks/Listing/HomePageTemplate.jsx
+++ src/components/Blocks/Listing/HomePageTemplate.jsx
@@ -4,8 +4,7 @@ import { ConditionalLink } from '@plone/volto/components';
 import { flattenToAppURL } from '@plone/volto/helpers';
 import { settings } from '~/config';
 
-import { Divider, Grid, Segment } from 'semantic-ui-react';
-import DefaultImageSVG from '@plone/volto/components/manage/Blocks/Listing/default-image.svg';
+import { Divider, Grid, Image, Segment } from 'semantic-ui-react';
 import { isInternalURL } from '@plone/volto/helpers/Url/Url';
 
 const DefaultTemplate = ({ items, linkMore, isEditMode }) => {
@@ -35,13 +34,12 @@ const DefaultTemplate = ({ items, linkMore, isEditMode }) => {
                 <Divider vertical />
                 <Grid.Row verticalAlign="middle">
                   <Grid.Column>
-                    <img
-                      src={flattenToAppURL(
-                        item[settings.listingPreviewImageField].scales.preview
-                          .download,
-                      )}
-                      alt={item.title}
+                    <Image
+                      size="small"
+                      alt={item.image_caption ? item.image_caption : item.title}
+                      src={item.image.scales.preview.download}
                     />
+
                   </Grid.Column>
 
                   <Grid.Column>
